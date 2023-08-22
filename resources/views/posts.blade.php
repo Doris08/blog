9776c978cc943d9180a247e53fcc30c49b95e8b2
Doris08 <akhino44@gmail.com>
2023-08-22 15:39:39 -0600

    Creacion de rutas y vistas para posts

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<title>
+    My blog
+</title>
+<link rel="stylesheet" href="/css/app.css">
+{{-- <script src="/js/app.js"></script> --}}
+<body>
+
+    <article>
+        <h1><a href="/posts/my-first-post">First article</a></h1>
+        <p>
+            fjvsij sijis jfiwsefiwsefiwsj efiwhs uf whiue huwes fwuhiquaw 
+            scjfsi dfcisuhfushfuhsfs iuhdfsuhfsu nfisifushfujsfhsdfisdfhshfis
+            sfjsoifjosfjoisdjgoisdjgoisxjosijgosjsjxoisjdoivgjsoxdj vfosfsjfois
+            scflaz jskidfsudfsifjoasijsoiefjsajoiaoifa oijaosj aoijsf jzvfijsz
+            cfjzsfvsjf vs efisu efiua heuweseu s7 fuhsif8use sahousfush h s fuhsif8
+            oisj fis iu ssi sdj is paji aufaiu poajsfoicsj zifuv hsidufs su vsuo od
+            podfsj fos fs sjoi jsdi sj ifjasfjsa ifjs s dos dfois fos hfu hsp`
+            sfosijfviuhiushdnigvh df vgd hdof jvg9doif
+        </p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-second-post">Second article</a></h1>
+        <p>
+            fjvsij sijis jfiwsefiwsefiwsj efiwhs uf whiue huwes fwuhiquaw 
+            scjfsi dfcisuhfushfuhsfs iuhdfsuhfsu nfisifushfujsfhsdfisdfhshfis
+            sfjsoifjosfjoisdjgoisdjgoisxjosijgosjsjxoisjdoivgjsoxdj vfosfsjfois
+            scflaz jskidfsudfsifjoasijsoiefjsajoiaoifa oijaosj aoijsf jzvfijsz
+            cfjzsfvsjf vs efisu efiua heuweseu s7 fuhsif8use sahousfush h s fuhsif8
+            oisj fis iu ssi sdj is paji aufaiu poajsfoicsj zifuv hsidufs su vsuo od
+            podfsj fos fs sjoi jsdi sj ifjasfjsa ifjs s dos dfois fos hfu hsp`
+            sfosijfviuhiushdnigvh df vgd hdof jvg9doif
+        </p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-third-post">Third article</a></h1>
+        <p>
+            fjvsij sijis jfiwsefiwsefiwsj efiwhs uf whiue huwes fwuhiquaw 
+            scjfsi dfcisuhfushfuhsfs iuhdfsuhfsu nfisifushfujsfhsdfisdfhshfis
+            sfjsoifjosfjoisdjgoisdjgoisxjosijgosjsjxoisjdoivgjsoxdj vfosfsjfois
+            scflaz jskidfsudfsifjoasijsoiefjsajoiaoifa oijaosj aoijsf jzvfijsz
+            cfjzsfvsjf vs efisu efiua heuweseu s7 fuhsif8use sahousfush h s fuhsif8
+            oisj fis iu ssi sdj is paji aufaiu poajsfoicsj zifuv hsidufs su vsuo od
+            podfsj fos fs sjoi jsdi sj ifjasfjsa ifjs s dos dfois fos hfu hsp`
+            sfosijfviuhiushdnigvh df vgd hdof jvg9doif
+        </p>
+    </article>
+
+</body>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
+});
+Route::get('posts/{post}', function ($slug) {
+    $path = __DIR__ . "/../resources/post/{$slug}.html";
+
+    if(! file_exists($path)){
+        //dd('file does not exist');
+        //abort(404);
+        return redirect('/');
+    }
+
+    $post = file_get_contents($path);
+
+    return view('post', [
+        'post' => $post
+    ]);
 });
