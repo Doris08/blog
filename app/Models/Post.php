1a362fd8a5947fd3290021637f50e2963d6276ad
Doris08 <akhino44@gmail.com>
2023-08-23 15:38:03 -0600

    Fin sección 2 The basis
    Agregando librería Spatie yaml-front-matter y cambios en modelo Post para mejorar métodos all usando colecciones  y find y además implementando cache y creación de atributos y método constructor de Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,25 +2,66 @@
 
 namespace App\Models;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post{
+
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+    public $slug;
+
+    public function __construct($title, $excerpt, $date, $body, $slug){
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
     public static function find($slug){
-        if(! file_exists($path = resource_path("post/{$slug}.html"))){
+
+        // Of all the blog posts, find the one with a slug that matches with the one that is requested
+        $posts = static::all();
+        
+        return $posts->firstWhere('slug', $slug);
+
+        // Una forma de hacerlo
+        /*if(! file_exists($path = resource_path("post/{$slug}.html"))){
             //dd('file does not exist');
             //throw new ModelNotFoundException();
             //throw new \Exception;
             abort(404);   
         }
 
-        return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
+        return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));*/
 
     }
 
     public static function all(){
 
-        $files = File::files(resource_path("post/"));
+        return cache()->rememberForever('posts.all', function(){
+            return collect(File::files(resource_path("post")))
+            ->map(function($file){ // usando función de flecha: ->map(fn($file) => YamlFrontMatter::parseFile($file))
+                return YamlFrontMatter::parseFile($file);
+            })
+            ->map(function($document){ // usando función de flecha: ->map(fn($document) => new Post(...)
+                return new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                );
+            })
+            ->sortByDesc('date'); //->sortBy('date'); ordena de forma ascendente
 
-        return array_map(fn($file) => $file->getContents(), $files);
+        });
+
+        // Otra forma de hacerlo
+        /*$files = File::files(resource_path("post/"));
+
+        return array_map(fn($file) => $file->getContents(), $files);*/
     
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,61 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+    // Lo ponemos dentro de la coleccion porque solo se usa una vez $files = File::files(resource_path("post"));
+    //$posts = [];
+
+    // Forma de hacerlo con collections, esta lógica se pasa al método all(); del modelo Post
+    /*$posts = collect(File::files(resource_path("post")))
+        ->map(function($file){ // usando función de flecha: ->map(fn($file) => YamlFrontMatter::parseFile($file))
+            return YamlFrontMatter::parseFile($file);
+        })
+        ->map(function($document){ // usando función de flecha: ->map(fn($document) => new Post(...)
+            return new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+            );
+        });*/
+
+    // Forma de hacerlo de forma un poco mas limpia para no usar foreach 
+    /*$posts = array_map(function($file){
+
+        $document = YamlFrontMatter::parseFile($file);
+        
+        return new Post(
+            $document->title,
+            $document->excerpt,
+            $document->date,
+            $document->body(),
+            $document->slug
+        );
+    }, $files);*/
+
+
+    // Forma de hacerlo con foreach y asignando un objeto Post a un array
+    /*foreach ($files as $file) {
+        $document = YamlFrontMatter::parseFile($file);
+        
+        $posts[] = new Post(
+            $document->title,
+            $document->excerpt,
+            $document->date,
+            $document->body(),
+            $document->slug
+        );
+    }*/
+
+    //ddd($posts[0]->body);
+
+    /*$document = YamlFrontMatter::parseFile(
+        resource_path('post/my-fourth-post.html')
+    );*/
+
+    //ddd($document->excerpt);
+
     return view('posts', [
         'posts' => Post::all()
     ]);
